6beb82a8f85530cb40d21df68969e906f762d03e
nicothe27 <nicothe27@gmail.com>
2025-08-14 10:04:15 -0300

    Agregamos la petición GET para obtener un solo contacto + Uso de HTTPEXception para el manejo de errores

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, HTTPException
 #Importamos managedb.py
 from src.lib.managedb import ManageDB
 
@@ -11,4 +11,16 @@ async def root():
 
 @app.get("/api/contacts")
 async def get_allcontacts():
-    return md.read_contacts()
+    return md.read_contacts() # Llamamos a la funcion read_contacts, usando la instancia de la clase ManageDB()
+
+
+@app.get("/api/contacts/{id_contact}")
+async def get_single_contact(id_contact:str):
+    # Busca y retorna el contacto cuyo id coincide con el valor recibido en la URL.
+    # Si no encuentra el contacto, no retorna nada !! (Falta agregar el mensaje de error)
+    contacts = md.read_contacts()
+    
+    for contact in contacts:
+        if contact["id"] == id_contact:
+            return(contact) 
+    raise HTTPException(status_code=404, detail="Contact not found")
